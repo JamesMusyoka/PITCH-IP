e428ee58b0f2f44b9e589837939f9b36fdaaf000
James Musyoka <jamesmu475@gmail.com>
2019-02-12 05:48:56 +0300

    updated it's functionality

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+
+class Config:
+
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:jamesmuitomusyoka@localhost/pitchip'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    DEBUG = True
+  
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:jamesmuitomusyoka@localhost/pitchip'
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:jamesmuitomusyoka@localhost/pitchip'
+    
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+
+}
