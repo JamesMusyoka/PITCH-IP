ef65c95d01fcfe8b96457b24e5638294b584b83e
James Musyoka <jamesmu475@gmail.com>
2019-02-14 12:05:05 +0300

    Indentation

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 class Config:
 
-    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
-    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    # MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    # MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:jamesmuitomusyoka@localhost/pitchip'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
