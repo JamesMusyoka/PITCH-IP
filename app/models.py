1ae8c79b1195d82fc4eaf73657d6556d9422488c
James Musyoka <jamesmu475@gmail.com>
2019-02-14 12:05:32 +0300

    updates

--- app/models.py
+++ app/models.py
@@ -1,44 +1,47 @@
-from . import db,login_manager
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
+     __tablename__ = 'users'
+     id = db.Column(db.Integer, primary_key=True)
+     username = db.Column(db.String(255))
 
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String(255), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
-    bio = db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
-    pitch = db.relationship('Review', backref='user', lazy="dynamic")
+     email = db.Column(db.String(255), unique=True, index=True)
+     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+     pass_secure = db.Column(db.String(255))
+     bio = db.Column(db.String(255))
+     profile_pic_path = db.Column(db.String())
+     password_secure = db.Column(db.String(255))
+     pitch = db.relationship('Review', backref='user', lazy="dynamic")
+      
+     def __repr__(self):
+         return f'User {self.username}'
 
-    @login_manager.user_loader
-    def load_user(user_id):
+     @login_manager.user_loader
+     def load_user(user_id):
         return User.query.get(int(user_id))
-    def save_user(self):
+     def save_user(self):
         db.session.add(self)
         db.session.commit()
     
 
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
+     @property
+     def password(self):
+         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
+     @password.setter
+     def password(self, password):
+         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-            return check_password_hash(self.pass_secure, password)
+     def check_password(self, password):
+             return check_password_hash(self.pass_secure, password)
 
-    def __repr__(self):
-        return f'User {self.username}'
+     def __repr__(self):
+         return f'User {self.username}'
 
 
 class Role(db.Model):
